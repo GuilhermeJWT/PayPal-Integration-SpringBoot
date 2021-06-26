7094f00dc72e59b5865584003b2d0a0019039eeb
GuilhermeJWT <guiromanno@gmail.com>
2021-06-26 13:50:29 -0300

    Criando nossa Página HTML com Thymeleaf para o Pagamento

--- src/main/java/br/com/systemsgs/controller/PayPalController.java
+++ src/main/java/br/com/systemsgs/controller/PayPalController.java
@@ -1,6 +1,7 @@
 package br.com.systemsgs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.systemsgs.service.PayPalService;
@@ -10,5 +11,10 @@ public class PayPalController {
 	
 	@Autowired
 	private PayPalService payPalService;
+	
+	@GetMapping(value = "/request")
+	public String init() {
+		return "paypal";
+	}
 
 }
